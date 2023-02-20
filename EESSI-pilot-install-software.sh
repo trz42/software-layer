a9622e254f514f7551f9360d8dbc539b6833c690
Richard <centos@vm-richard.novalocal>
2023-02-20 08:22:43 +0000

    Add OpenMPI/4.1.1 with GCC/10.3.0 to NESSI/2022.11

--- EESSI-pilot-install-software.sh
+++ EESSI-pilot-install-software.sh
@@ -450,6 +450,7 @@ $EB CMake-3.20.1-GCCcore-10.3.0.eb --robot --include-easyblocks-from-pr 2248
 ### add packages here
 #####################
 $EB Python-3.9.5-GCCcore-10.3.0.eb --robot
+$EB OpenMPI-4.1.1-GCC-10.3.0.eb  --robot 
 # example block showing a few debugging means
 #echo "Installing CaDiCaL/1.3.0 for GCC/9.3.0..."
 #ok_msg="CaDiCaL installed. Nice!"
