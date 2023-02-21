4adff923981d43ec6641c10d5414fb1c4c9c21db
Thomas Roeblitz <thomas.roblitz@uib.no>
2023-02-21 21:39:14 +0100

    implement a different procedure to install apptainer, reenable tests

--- install_apptainer_ubuntu.sh
+++ install_apptainer_ubuntu.sh
@@ -2,13 +2,22 @@
 
 set -e
 
-# see https://github.com/apptainer/singularity/issues/5390#issuecomment-899111181
-sudo apt-get install alien
-alien --version
-apptainer_rpm=$(curl --silent -L https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/a/ | grep 'apptainer-[0-9]' | sed 's/.*\(apptainer[0-9._a-z-]*.rpm\).*/\1/g')
-curl -OL https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/a/${apptainer_rpm}
-sudo alien -d ${apptainer_rpm}
-sudo apt install ./apptainer*.deb
+# old method, see https://github.com/apptainer/singularity/issues/5390#issuecomment-899111181
+#sudo apt-get install alien
+#alien --version
+#apptainer_rpm=$(curl --silent -L https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/a/ | grep 'apptainer-[0-9]' | sed 's/.*\(apptainer[0-9._a-z-]*.rpm\).*/\1/g')
+#curl -OL https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/a/${apptainer_rpm}
+#sudo alien -d ${apptainer_rpm}
+#sudo apt install ./apptainer*.deb
+
+# new method, see https://apptainer.org/docs/admin/latest/installation.html#install-debian-packages
+sudo apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository -y ppa:apptainer/ppa
+sudo apt update
+sudo apt install -y apptainer
+
+# test versions installed
 apptainer --version
 # also check whether 'singularity' command is still provided by Apptainer installation
 singularity --version
