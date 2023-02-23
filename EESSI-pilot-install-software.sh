1a017cd007f19746b94fc6a6563de5236dc47877
parosen <parosen@uio.no>
2023-02-23 11:38:43 +0100

    Adding QuantumESPRESSO to NESSI/2022.11

--- EESSI-pilot-install-software.sh
+++ EESSI-pilot-install-software.sh
@@ -315,13 +315,13 @@ echo_green "All set, let's start installing some software in ${EASYBUILD_INSTALL
 #$EB OpenFOAM-8-foss-2020a.eb OpenFOAM-v2006-foss-2020a.eb --robot
 #check_exit_code $? "${ok_msg}" "${fail_msg}"
 
-#if [ ! "${EESSI_CPU_FAMILY}" = "ppc64le" ]; then
-#    echo ">> Installing QuantumESPRESSO..."
-#    ok_msg="QuantumESPRESSO installed, let's go quantum!"
-#    fail_msg="Installation of QuantumESPRESSO failed, did somebody observe it?!"
-#    $EB QuantumESPRESSO-6.6-foss-2020a.eb --robot
-#    check_exit_code $? "${ok_msg}" "${fail_msg}"
-#fi
+if [ ! "${EESSI_CPU_FAMILY}" = "ppc64le" ]; then
+    echo ">> Installing QuantumESPRESSO..."
+    ok_msg="QuantumESPRESSO installed, let's go quantum!"
+    fail_msg="Installation of QuantumESPRESSO failed, did somebody observe it?!"
+    $EB QuantumESPRESSO-6.6-foss-2020a.eb --robot
+    check_exit_code $? "${ok_msg}" "${fail_msg}"
+fi
 
 #echo ">> Installing R 4.0.0 (better be patient)..."
 #ok_msg="R installed, wow!"
