8a181115fc051d82e8365edb40b8a400a86d4568
Richard <centos@vm-richard.novalocal>
2023-02-27 07:35:45 +0000

    Add OSU-Micro-Benchmarks/5.6.3 with GCC/9.3.0 to NESSI/2022.11

--- EESSI-pilot-install-software.sh
+++ EESSI-pilot-install-software.sh
@@ -365,7 +365,7 @@ echo_green "All set, let's start installing some software in ${EASYBUILD_INSTALL
 #echo ">> Installing OSU-Micro-Benchmarks 5.6.3..."
 #ok_msg="OSU-Micro-Benchmarks installed, yihaa!"
 #fail_msg="Installation of OSU-Micro-Benchmarks failed, that's unexpected..."
-#$EB OSU-Micro-Benchmarks-5.6.3-gompi-2020a.eb -r
+$EB OSU-Micro-Benchmarks-5.6.3-gompi-2020a.eb -r
 #check_exit_code $? "${ok_msg}" "${fail_msg}"
 
 #echo ">> Installing Spark 3.1.1..."
