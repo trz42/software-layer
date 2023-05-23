fb31a1a2b5f2786c34f8f22743e858a0d7c2d377
Richard Top <richard.topouchian@uib.no>
2023-05-23 08:35:56 +0000

    Adding special handling of GENERIC CPU targets without local easyblocks

--- EESSI-pilot-install-software.sh
+++ EESSI-pilot-install-software.sh
@@ -212,18 +212,16 @@ echo_green "All set, let's start installing some software in ${EASYBUILD_INSTALL
 
 # add OpenBLAS: requires special handling for GENERIC CPU targets
 # If we're building OpenBLAS for GENERIC, we need https://github.com/easybuilders/easybuild-easyblocks/pull/1946
-# we need to specify --parallel 1 or run into weird linking issues supposedly
-# introduced by GNU make v4.4, see for example
+# we need to use the fix provided with https://github.com/easybuilders/easybuild-easyconfigs/pull/17924
+# which used make v4.3 as build dependency
+# else we run into weird linking issues introduced by GNU make v4.4, see for example
 # https://github.com/xianyi/OpenBLAS/issues/3899 and https://github.com/xianyi/OpenBLAS/issues/3979
-# a fix may be provided with https://github.com/xianyi/OpenBLAS/pull/3983
-# the fix did not work, so only option seems to be --parallel 1
-# another option seems to be to use make v4.3 as build dependency
 if [[ $GENERIC -eq 1 ]]; then
     echo ">> Installing OpenBLAS..."
     ok_msg="Done with OpenBLAS (GENERIC architecture)!"
     fail_msg="Installation of OpenBLAS (GENERIC architecture) failed!"
     echo_yellow ">> Using https://github.com/easybuilders/easybuild-easyblocks/pull/1946 to build generic OpenBLAS."
-    $EB --include-easyblocks easyblocks/o/openblas-pr1946-cc74e45.py OpenBLAS-0.3.15-GCC-10.3.0.eb --robot --from-pr 17924
+    $EB --include-easyblocks-from-pr 1946  OpenBLAS-0.3.15-GCC-10.3.0.eb --robot --from-pr 17924
     check_exit_code $? "${ok_msg}" "${fail_msg}"
 fi
 
