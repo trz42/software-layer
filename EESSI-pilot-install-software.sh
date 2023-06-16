8abf2c2d0d2f6900a20af7b41b23be899ef16611
Thomas Roeblitz <thomas.roblitz@uib.no>
2023-06-16 16:52:37 +0200

    prepare installation of software + rename old easystack file

--- EESSI-pilot-install-software.sh
+++ EESSI-pilot-install-software.sh
@@ -143,7 +143,20 @@ for eb_version in '4.7.2'; do
 
     echo_green "All set, let's start installing some software with EasyBuild v${eb_version} in ${EASYBUILD_INSTALLPATH}..."
 
-    ${TOPDIR}/check_missing_installations.sh
+    for gen in '2021a'; do
+
+        es="eessi-${EESSI_PILOT_VERSION}-eb-${eb_version}-${gen}.yml"
+
+        if [ -f ${es} ]; then
+            echo_green "Feeding easystack file ${es} to EasyBuild..."
+
+            ${EB} --easystack ${TOPDIR}/${es} --missing && ${EB} --easystack ${TOPDIR}/${es} --robot
+
+            $TOPDIR/check_missing_installations.sh ${TOPDIR}/${es}
+        else
+            fatal_error "Easystack file ${es} not found!"
+        fi
+    done
 done
 
 echo ">> Creating/updating Lmod cache..."
